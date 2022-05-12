2c5bdb8ebbf1f95d1de43574e4428927ee829a6b
JOnathanBrasil <j_jonatha_2011@hotmail.com>
2022-05-12 17:39:59 -0300

    adicionando métodos de buscar todos os usuários e buscar por ID

--- src/main/java/com/generation/lojabign/controller/UsuarioController.java
+++ src/main/java/com/generation/lojabign/controller/UsuarioController.java
@@ -1,5 +1,56 @@
 package com.generation.lojabign.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.lojabign.model.Usuario;
+import com.generation.lojabign.model.UsuarioLogin;
+import com.generation.lojabign.repository.UsuarioRepository;
+import com.generation.lojabign.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/all")
+	public ResponseEntity <List<Usuario>> getAll(){
+		return ResponseEntity.ok(usuarioRepository.findAll());
+		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity <Usuario> getById (@PathVariable Long id){
+		return usuarioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+		
+	}
+	
+	@PostMapping ("/logar")
+	public ResponseEntity <UsuarioLogin> login (@RequestBody Optional<UsuarioLogin> usuarioLogin){
+		return usuarioService.autenticarUsuario(usuarioLogin)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+		
+	}
+	
+	
 
 }
